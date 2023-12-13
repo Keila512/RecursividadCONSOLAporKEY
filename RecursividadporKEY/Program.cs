2bcc598c3cefe3c9823db6725aae6f0f63f9ce54
JoseFabela <jfabela522@gmail.com>
2023-12-13 14:44:55 -0600

    Agregar archivos de proyecto.

---
+++ RecursividadporKEY/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursividadporKEY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\"; 
+            ListFilesRecursively(directoryPath);
+            Console.ReadLine();
+        }
+        static void ListFilesRecursively(string directoryPath)
+        {
+            try
+            {
+                foreach (string file in Directory.GetFiles(directoryPath))
+                {
+                    Console.WriteLine($"ARCHIVO: {Path.GetFileName(file)}, EXTENSION: {Path.GetExtension(file)}");
+                }
+
+                foreach (string subdirectory in Directory.GetDirectories(directoryPath))
+                {
+                    ListFilesRecursively(subdirectory);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"ERROR: Error al acceder a {directoryPath}: {e.Message}");
+            }
+        }
+    }
+}
